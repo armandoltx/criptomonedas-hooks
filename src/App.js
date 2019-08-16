eb6749cde904603ac5b05aa9b50737f708134bd4
Armando Ruiz <armando.ruiz@industrie.co>
2019-08-16 11:20:14 +1000

    Agregando el Spinner y mostrandolo al hacer la consulta

--- src/App.js
+++ src/App.js
@@ -2,27 +2,44 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import imagen from './cryptomonedas.png';
 import Formulario from './components/Formulario';
+import Spinner from './components/Spinner';
 
 function App() {
   // state para traernos los valores de la moneda y la criptomoneda al componenete principal
   const [ moneda, guardarMoneda ] = useState('');
-  const [criptomoneda, guardarCriptomoneda] = useState();
+  const [ criptomoneda, guardarCriptomoneda ] = useState();
+  // anadimos otro state para el spinner
+  const [ cargando, guardarCargando ] = useState(false);
 
   // pedriamos crear una funcion para pasar guardarMoneda y guardarCriptomoneda al compoenente formulario, pero lo podemos pasar directamente y hacer destructuring en el componente formulario
 
   useEffect(
     () => {
+      // para que no se ejecute inmediatamente al iniciar la web si no hay moneda, q no se ejecute
+      if(moneda === '' || criptomoneda === '') return;
+
       const cotizarCriptoMoneda = async () => {
         const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
 
         const resultado = await axios.get(url);
         console.log("resulado ", resultado);
+
+        // cuando hacemos la consulta que se vea el spinner
+        guardarCargando(true);
+
+        setTimeout(
+          () => {
+            guardarCargando(false);
+          }, 3000);
       }
 
       cotizarCriptoMoneda();
 
     }, [criptomoneda, moneda]
   );
+
+  // Mostrar el spinner o el resultado
+  const componente = (cargando) ? <Spinner /> : null;
 
   return (
     <div className="container">
@@ -36,6 +53,8 @@ function App() {
             guardarMoneda={guardarMoneda}
             guardarCriptomoneda={guardarCriptomoneda}
           />
+
+          {componente}
         </div>
       </div>
     </div>
